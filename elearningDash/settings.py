566d23453c80db8682de793351c94833e62b883a
minna123-cap <minura.samaranayake@gmail.com>
2020-12-12 12:13:58 +0530

    Update settings.py

--- elearningDash/settings.py
+++ elearningDash/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'elearningDash.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "Templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
