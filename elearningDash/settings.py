c59c28524c17e6e0ba965d44354a2706e48b9ce2
minna123-cap <minura.samaranayake@gmail.com>
2020-12-12 15:46:21 +0530

    cds

--- elearningDash/settings.py
+++ elearningDash/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/image/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
